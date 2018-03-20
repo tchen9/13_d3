82670f3ec90dc618b06c1272b34be23eca864847
angelng1412 <ang7@stuy.edu>
2018-03-20 05:21:51 -0400

    issues fixed

--- plot.js
+++ plot.js
@@ -1,11 +1,11 @@
 var svg = document.getElementById("s");
-var info = [ [5, 60], [5, 62], [6, 63], [7, 65], [7, 67], [9, 69], [10, 70]];
+var info = [[5, 60], [5, 62], [6, 63], [7, 65], [7, 67], [9, 69], [10, 70]];
 
 var scatterplot = function(){
     var container = d3.select("svg");
     var circles = container.selectAll("circle").data(info).enter();
     circles.append("circle")
-	.attr("cx", function(d) {return d[0] * 50})
+	.attr("cx", function(d) {return (d[0] * 50)+50})
 	.attr("cy", function(d) {return (d[1]/100)*(-400)+450})
 	.attr("r", 5)
 	.attr("fill", "lightsteelblue");
